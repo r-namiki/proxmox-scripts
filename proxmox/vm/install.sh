9899f45464410cecaaa134683226cd8d3881d7b0
r-namiki <r.namiki.leisure@gmail.com>
2025-10-13 01:42:21 +0900

    fix: vmのinstall.shをubuntu用に修正

--- proxmox/vm/install.sh
+++ proxmox/vm/install.sh
@@ -19,12 +19,12 @@ sudo apt-get install -y \
 
 echo ""
 echo "Adding Docker GPG key..."
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo ""
 echo "Adding Docker repository..."
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo ""
